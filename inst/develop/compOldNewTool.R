14bbd01425a11b12242bc7fdf26379b95a353adb
Antje Maria Moffat <amoffat@bgc>
2014-01-15 22:44:42 +0100

    Scripts to compare new and old tool

---
+++ inst/develop/compOldNewTool.R
@@ -0,0 +1,77 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++ Developers' R script +++
+#+++ Compare old and new tool and Fluxnet
+#+++ It is important to perform the new algorithm on the same subset of data after filtering.
+#+++ Therefore the new algorithm has to be performed AFTER the old pv-wave algorithm.
+#+++ --> run from parent directory
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Author: AMM
+
+
+# Generate output data from PV-Wave webtool: http://www.bgc-jena.mpg.de/~MDIwork/eddyproc/
+# 1. Generate files with data format of old PV-Wave tool using the script 'genOldToolData.R'
+# 2. Load up data in online tool input form with the following settings:
+#   Year=..., Time Format 'Julian day, hour', Delimter <TAB>, GF with Uncertainties? <YES>, u*-Filtering before GF? <YES>, 
+#   Standard FP? <YES>, GL2010 FP? <NO>, Temperature Variable used for FP? <Tair>, Latitude: ..., Email Address: ...
+# 3. Load data from old online tool output file <..._DataSetafterFluxpart.txt>
+# 4. Use this data to run new flux partitioning algorithm. This ensures that the algorithms are based on the SAME subset of (filtered) NEE data.
+# 5. Compare results...
+         
+# Source settings for R environment and standard functions
+# (Cleans up workspace and set directory paths)
+source('inst/develop/setREnvir.R') 
+require('REddyProc')
+
+#!!! source because of new function
+source('~/Projects/REddy/REddyProc/R/FileHandling.R')
+
+# Result files with old tool pv-wave output data
+ResultsFile.V.s <- fInitFilesDir('out', 'DataSetafterFluxpart.txt')
+ResultsName.V.s <- fStripFileExtension(SiteFile.V.s) #!!! Programm to be only AT-Neu...
+ResultsName.V.s <- c('DE-Tha')
+
+#Fluxnet files for site info
+FluxFile.V.s <- fInitFilesDir(DirFluxnet.s, 'hourly.nc')
+FluxName.V.s <- fStripFileExtension(FluxFile.V.s)
+
+
+for (Result.i in 1:length(ResultName.V.s)) {
+  #Result.i <- 1
+  message(paste('Handling site \'', ResultsName.V.s[Result.i],'\'', sep=''))
+  FluxSite.i <- which(FluxName.V.s == ResultsName.V.s[Result.i])
+  
+  # Load old tool pv-wave output data
+  if( sum(grepl('OldToolData.F', ls(all=TRUE))) == 0 ) # Load only OldToolData.F, if not yet loaded
+    OldToolData.F <- fLoadTXTIntoDataframe('Example_DETha98_PVWave_DataSetafterFluxpart.txt', 'inst/examples') #takes a minute or two
+  
+  # Convert data frame to comply with REddyProc format
+  PVData.F <- fConvertTimeToPosix(OldToolData.F, 'YMDHM', Year.s = 'Year', Month.s= 'Month', Day.s = 'Day', Hour.s = 'Hour', Min.s = 'Minute')
+  PVData.F$sDateTime <- PVData.F$DateTime
+  
+  # Rename complicated names from webtool
+  PVData.F$R_ref <- PVData.F$Rrefopt_OrdE0_2_from
+  PVData.F$E_0 <- PVData.F$E0_2_from_Tair
+  
+  #Load site information from BGC nc files (used to generate old tool input data)
+  Info.L <- fLoadFluxNCInfo(FluxFile.V.s[FluxSite.i], DirFluxnet.s)
+  
+  # Run new flux partitioning algorithm 
+  # Automatically uses the NEE_f, NEE_fqc, Tair_f, Tair_fqc, and Rg from the pv-wave output files
+  # This ensures that the algorithms are based on the SAME subset of (filtered) NEE data
+  EPSite.C <- sEddyProc$new(ResultsName.V.s[Result.i], PVData.F, c('NEE_f', 'NEE_fqc', 'Tair_f', 'Tair_fqc', 'Rg'))
+  EPSite.C$sMRFluxPartition(Lat_deg.n=Info.L$LAT, Long_deg.n=Info.L$LON, TimeZone_h.n=Info.L$TZ)
+  NewFPResults.F <- EPSite.C$sExportResults()
+  
+  # Very basic plot of differences to BGI fluxnet version
+  # Plot 1998 ...
+  #plot(EddyNCData.F$Reco[35089:52608] ~ PVData.F$Reco, col=rgb(0.4,0.4,0.4,alpha=0.2), pch=20, cex=0.3, main='PV Fluxnet vs. PV Webtool')
+  plot(NewFPResults.F$Reco ~ PVData.F$Reco, col=rgb(0.4,0.4,0.4,alpha=0.2), pch=20, cex=0.3, main='R code vs. PV Webtool')
+  # Residuals R_eco
+  plot(PVData.F$Reco-PVData.F$Reco, col='red', pch=20, cex=0.3, ylim=c(-3,3))
+  #points(EddyNCData.F$Reco[35089:52608]-PVData.F$Reco, col='violet', pch=20, cex=0.3)
+  points(NewFPResults.F$Reco-PVData.F$Reco, col='blue', pch=20, cex=0.3)
+  legend("topright", legend=c('PV Webtool','PV Fluxnet','R Code'), pch=20, col=c('red','violet','blue'))
+  # --> Larger differences of PV BGI Fluxnet with PV Webtool than R code :-) !
+  
+  
+}
---
+++ inst/develop/genOldToolData.R
@@ -0,0 +1,70 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++ Developers' R script +++
+#+++ Generate input data for the old online tool (based on pv-wave) from BGI fluxnet data
+#+++ to be used in comparison compOldNewTool.R
+#+++ (Main formatting differences old to new tool: single years only, (no year column), 'Day' -> 'DoY')
+#+++ --> run from parent directory
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Author: AMM
+
+# Source settings for R environment and standard functions
+# (Cleans up workspace and set directory paths)
+source('inst/develop/setREnvir.R') 
+require('REddyProc')
+
+#!!!
+DirFluxnet.s <- paste('~/Data/Fluxnet/level5_new_nc') #!!! Quick fix to work from home, comment out later...
+
+#List of site and years to compare
+CompList.V.s <- c('AT-Neu.2004','BR-Ma2.2005','BR-Sa1.2003','CA-Let.2000','CA-NS7.2004','CA-TP3.2005',
+                   'CH-Oe1.2004','CH-Oe2.2004','CN-HaM.2003','DE-Hai.2002','DE-Tha.1998','DK-Sor.2006',
+                   'ES-ES1.2000','ES-VDA.2005','FI-Hyy.1998','FI-Kaa.2001','FR-Fon.2006','FR-Gri.2006',
+                   'FR-Hes.1998','FR-Lq1.2006','FR-Lq2.2006','FR-Pue.2003','IE-Dri.2004','IL-Yat.2005',
+                   'IT-Amp.2004','IT-MBo.2005','IT-Pia.2004','IT-SRo.2001','JP-Tef.2005','PT-Esp.2004',
+                   'RU-Cok.2004','SE-Deg.2003','SE-Nor.1997','US-Ha1.2000','US-MMS.2004','US-SO2.2006',
+                   'US-Ton.2004','VU-Coc.2002','ZA-Kru.2002')
+CompName.V.s <- fStripFileExtension(CompList.V.s)
+
+# List of files in FLUXNET directory
+FluxFile.V.s <- fInitFilesDir(DirFluxnet.s, 'hourly.nc')
+FluxName.V.s <- fStripFileExtension(FluxFile.V.s)
+
+for (CompSite.i in 1:length(CompName.V.s)) {
+  #CompSite.i=11
+  FluxSite.i <- which(FluxName.V.s == CompName.V.s[CompSite.i])
+  Year.i <- as.numeric(sub('.*[.]','',CompList.V.s[CompSite.i]))
+  message(paste('Handling site file \"', FluxFile.V.s[FluxSite.i],'\" for the year ', Year.i, sep=''))
+  
+  #Load data from NetCDF
+  # Variables needed for running pv-wave tool
+  VarPV.V.s <- c('NEE', 'LE', 'H', 'Rg', 'VPD', 'rH', 'Tair', 'Tsoil_f', 'julday') #Attention: Rh and rH for different version of BGI processing
+  VarPV.V.s <- c('NEE', 'LE', 'H', 'Rg', 'VPD', 'Rh', 'Tair', 'Tsoil_f', 'julday') #!!!delete
+  # Variables needed to also compare to Fluxnet (FX) version
+  VarFX.V.s <- c('Tair_f', 'Tair_fqcOK', 'NEE_f', 'NEE_fqc', 'Rg_pot', 'Reco', 'GPP_f')
+  VarFX_new.V.s <- NULL
+  #VarFX_new.V.s <- c('FX_Tair_f', 'FX_Tair_fqcOK', 'FX_NEE_f', 'FX_NEE_fqc', 'FX_Rg_pot', 'FX_Reco', 'FX_GPP_f')
+  EddyNCData.F <- NULL #Reset
+  EddyNCData.F <- fLoadFluxNCIntoDataframe(c(VarPV.V.s,VarFX.V.s), FluxFile.V.s[FluxSite.i], DirFluxnet.s) #takes longer...
+  
+  # Set to single year
+  SiteData.F <- EddyNCData.F
+  DTS.n <- 48 #half-hourly data
+  TimeShift.n <- (0.5 * 24/DTS.n * 60 * 60)  #half-period time offset in seconds to get correct years (usually internal...)
+  SiteData.F <-SiteData.F[as.numeric(format(SiteData.F$DateTime-TimeShift.n,"%Y"))==Year.i,]
+  
+  # Format necessary column for input file
+  # Rename Fluxnet columns
+  names(SiteData.F)[names(SiteData.F) %in% VarFX.V.s] <- VarFX_new.V.s
+  # Rename Tsoil_f to pretend it is unfilled (no fqc available)
+  names(SiteData.F)[names(SiteData.F)=='Tsoil_f'] <- 'Tsoil'
+  # Set PV-Wave time stamp at front
+  SiteData.F <- cbind(qcNEE=c(1), SiteData.F) #Just a dummy
+  SiteData.F <- cbind(Hour=as.numeric(format(SiteData.F$DateTime, '%H')) + as.numeric(format(SiteData.F$DateTime, '%M'))/60, SiteData.F)
+  SiteData.F <- cbind(Day=SiteData.F$julday, SiteData.F) #With funny uncorrect formatting...
+  #Otherwise it would be: cbind(Day=as.numeric(format(SiteData.F$DateTime, '%j')), SiteData.F) #Attention: Called day but is actually DoY
+  # Remove DateTime stamp and Fluxnet time columns
+  SiteData.F <- SiteData.F[,!names(SiteData.F) %in% c('DateTime','year','month','day','hour','julday')]
+  
+  FileName.s <- paste(CompList.V.s[CompSite.i], '.txt', sep='')
+  fWriteDataframeToFile(SiteData.F, FileName.s,'out')
+}
