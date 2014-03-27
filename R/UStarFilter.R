4d24b60a18f1eb5dbcfe35cb2ef4b2635b3a4071
twutz <devnull@localhost>
2014-03-27 09:56:08 +0100

    harmonize case of UStar

--- R/UStarFilter.R
+++ R/UStarFilter.R
@@ -1,13 +1,13 @@
 # TODO: Is the library(plyr) already loaded?
 # needed for function arrange()
 
-controluStarEst <- function(
+controlUstarEst <- function(
   #T Classes not needed here?
   # either?
   #,taClasses=7 # set number of ta classes   
   # or?
-  #,ctrluStarSubsetting.l = controluStarSubsetting()
-  #,taClasses=ctrluStarSubsetting.l$taClasses  
+  #,ctrlUstarSubsetting.l = controlUstarSubsetting()
+  #,taClasses=ctrlUstarSubsetting.l$taClasses  
   #
   #,percentile = 90 #percentile value... double check!
   #,percentile_check = TRUE #enable percentile check\n ... double check!
@@ -29,13 +29,13 @@ controluStarEst <- function(
   #define MIN_VALUE_SEASON				160			/* min for seasons */
   #define TA_CLASS_MIN_SAMPLE				100
   #TODO: what does the following param do?
-  #define FIRST_uStar_MEAN_CHECK  		0.2  
+  #define FIRST_Ustar_MEAN_CHECK  		0.2  
   # 4.) const int percentiles[PERCENTILES_COUNT] = { 1, 5, 10, 25, 50, 75, 90, 95, 99 };
   
 ){
   ctrl <- list(  
     #taClasses=taClasses
-    #,uStarClasses=uStarClasses  
+    #,UstarClasses=UstarClasses  
     #percentile = percentile
     #percentile_check = percentile_check #enable percentile check\n ... double check!
     ustPlateauFwd = ustPlateauFwd #number of subsequent thresholds to compare to in fwd mode
@@ -53,54 +53,54 @@ controluStarEst <- function(
   if (corrCheck != 0.5) warning("WARNING: parameter corrCheck set to non default value!")
   ctrl
 }
-attr(controluStarEst,"ex") <- function(){
-	controluStarEst()
+attr(controlUstarEst,"ex") <- function(){
+	controlUstarEst()
 }
 
-controluStarSubsetting <- function(
+controlUstarSubsetting <- function(
   	taClasses=7 		##<< set number of air temperature classes 
-	,uStarClasses=20 	##<< set number of uStar classes 	
+	,UstarClasses=20 	##<< set number of Ustar classes 	
 ){  
   ctrl <- list(
     taClasses=taClasses
-	,uStarClasses= uStarClasses
+	,UstarClasses= UstarClasses
   )	
   if (taClasses != 7) warning("WARNING: parameter taClasses set to non default value!")	
-  if (uStarClasses != 20) warning("WARNING: parameter uStarClasses set to non default value!")
+  if (UstarClasses != 20) warning("WARNING: parameter UstarClasses set to non default value!")
   ctrl
 }
-attr(controluStarSubsetting,"ex") <- function(){
-	controluStarSubsetting()
+attr(controlUstarSubsetting,"ex") <- function(){
+	controlUstarSubsetting()
 }
 
-binuStar <- function(
+binUstar <- function(
 	# bin the 
 	NEE.v				##<< vector with value of Net Ecosystem exchange
-	,uStar.v 			##<< vector with u* (friction velocity (m2/s)
-	,ctrluStarEst.l = controluStarEst()
+	,Ustar.v 			##<< vector with u* (friction velocity (m2/s)
+	,ctrlUstarEst.l = controlUstarEst()
 ){
-	ds.f <- data.frame(NEE=NEE.v,uStar=uStar.v)
+	ds.f <- data.frame(NEE=NEE.v,Ustar=Ustar.v)
 	
-	#within data frame sort values by uStar
+	#within data frame sort values by Ustar
 	ds.f <- arrange(ds.f,ds.f[,2])
 	
 	N_T <- length(NEE.v) #number of observations(rows) in a T class
-	Ust_bin_size <- round(N_T/ctrluStarEst.l$uStarClasses)
+	Ust_bin_size <- round(N_T/ctrlUstarEst.l$UstarClasses)
 	
-	#set up data frame for bin averages (by uStar)
-	Ust_bins.m <- matrix(NA,ctrluStarEst.l$uStarClasses,2)
+	#set up data frame for bin averages (by Ustar)
+	Ust_bins.m <- matrix(NA,ctrlUstarEst.l$UstarClasses,2)
 	Ust_bins.f <- data.frame(Ust_bins.m)
 	names(Ust_bins.f)[1]="Ust_avg"; names(Ust_bins.f)[2]="NEE_avg";
 	#names(Ust_bins.f)[3]="N";
 	
 	#calculate u* bin averages
-	for (u in 1:ctrluStarEst.l$uStarClasses){
+	for (u in 1:ctrlUstarEst.l$UstarClasses){
 		#/om:this part only implemented for checking C code compatibility...
 		#ust_class_start = ust_class_end
 		#ust_class_end = ust_class_start + Ust_bin_size-1
 		#dataUstclass <- dataTclass[ust_class_start:ust_class_end,]
 		#/eom    
-		if (u==ctrluStarEst.l$uStarClasses){ 
+		if (u==ctrlUstarEst.l$UstarClasses){ 
 			# use end index of vector for slightly smaller last bin (due to rounding) 
 			dsUstClass.f <- ds.f[((u-1)*Ust_bin_size+1):N_T,]
 		}
@@ -108,27 +108,27 @@ binuStar <- function(
 			dsUstClass.f <- ds.f[((u-1)*Ust_bin_size+1):((u)*Ust_bin_size),]
 		}
 		#TODO: merge two following lines to one
-		Ust_bins.f$NEE_avg[u] <- mean(dsUstClass.f[,1], na.rm=T) #mean of NEE over uStar bins
-		Ust_bins.f$Ust_avg[u] <- mean(dsUstClass.f[,2], na.rm=T) #mean of uStar bins
+		Ust_bins.f$NEE_avg[u] <- mean(dsUstClass.f[,1], na.rm=T) #mean of NEE over Ustar bins
+		Ust_bins.f$Ust_avg[u] <- mean(dsUstClass.f[,2], na.rm=T) #mean of Ustar bins
 		#Ust_bins.f$N[u] <- sum(!is.na(dataUstclass$nee))
 	}
 	Ust_bins.f
 	
 }
-attr(binuStar,"ex") <- function(){
+attr(binUstar,"ex") <- function(){
 	Dir.s <- paste(system.file(package='REddyProc'), 'examples', sep='/')
 	EddyData.F <- fLoadTXTIntoDataframe('Example_DETha98.txt', Dir.s)
 	dss <- subset(EddyData.F, DoY >= 150 & DoY <= 250)
-	(res <- binuStar(dss$NEE, dss$Ustar))
-	(resFW1 <- estuStarThresholdSingleFw1Binned(res))
-	(resFW2 <- estuStarThresholdSingleFw1Binned(res))
+	(res <- binUstar(dss$NEE, dss$Ustar))
+	(resFW1 <- estUstarThresholdSingleFw1Binned(res))
+	(resFW2 <- estUstarThresholdSingleFw1Binned(res))
 }
 
 
-estuStarThresholdSingleFw1Binned <- function(
-		### estimate the uStar threshold for single subset, using FW1 algorithm, relying on binned NEE and uStar
-		Ust_bins.f			##<< data.frame with column s NEE_avg and Ust_avg, of NEE by uStar bins by \code{\link{binuStar}}
-		,ctrluStarEst.l = controluStarEst()
+estUstarThresholdSingleFw1Binned <- function(
+		### estimate the Ustar threshold for single subset, using FW1 algorithm, relying on binned NEE and Ustar
+		Ust_bins.f			##<< data.frame with column s NEE_avg and Ust_avg, of NEE by Ustar bins by \code{\link{binUstar}}
+		,ctrlUstarEst.l = controlUstarEst()
 ){
 	##references<< inspired by Papale 2006
 	
@@ -140,53 +140,53 @@ estuStarThresholdSingleFw1Binned <- function(
 	# in order to avoid infinite loop in case of error
 	# optimize with Thomas?
 	while (!flag){ #only stop if threshold is found
-		if (!flag & (Ust_bins.f$NEE_avg[u] >= (ctrluStarEst.l$plateauCrit*mean(Ust_bins.f$NEE_avg[(u+1):(u+ctrluStarEst.l$ustPlateauFwd)],na.rm=T)))){ #na.rm=T to exclude NAs out of bounds..
+		if (!flag & (Ust_bins.f$NEE_avg[u] >= (ctrlUstarEst.l$plateauCrit*mean(Ust_bins.f$NEE_avg[(u+1):(u+ctrlUstarEst.l$ustPlateauFwd)],na.rm=T)))){ #na.rm=T to exclude NAs out of bounds..
 			#   NEE_i >= .95*avg(i,i+1,...,i+10)  [FW]    
-			uStarThSingle <- Ust_bins.f$Ust_avg[u]        
+			UstarThSingle <- Ust_bins.f$Ust_avg[u]        
 			flag <- TRUE #set flag for threshold found in this mode
 		}          
 		#case that no threshold could be found by plateau method, use maximum u* in that T_class...
 		if (u==(nrow(Ust_bins.f)-1)){ #FW1: -1 ; FW2: 
-			uStarThSingle <- Ust_bins.f$Ust_avg[u+1]        
+			UstarThSingle <- Ust_bins.f$Ust_avg[u+1]        
 			break;      
 		}
 		u <- u+1 #increase index by 1
 	}  
-	return(uStarThSingle)
+	return(UstarThSingle)
 }
 
 
 
 
-.depr.estuStarThresholdSingleFw1 <- function(
-  ### estimate the uStar threshold for single subset, using FW1 algorithm		
+.depr.estUstarThresholdSingleFw1 <- function(
+  ### estimate the Ustar threshold for single subset, using FW1 algorithm		
   NEE.v				##<< vector with value of Net Ecosystem exchange
-  ,uStar.v 			##<< vector with u* (friction velocity (m2/s)
-  ,ctrluStarEst.l = controluStarEst()
+  ,Ustar.v 			##<< vector with u* (friction velocity (m2/s)
+  ,ctrlUstarEst.l = controlUstarEst()
 ){
   ##references<< inspired by Papale 2006
-  ds.f <- data.frame(NEE.v,uStar.v)
+  ds.f <- data.frame(NEE.v,Ustar.v)
   
-  #within data frame sort values by uStar
+  #within data frame sort values by Ustar
   ds.f <- arrange(ds.f,ds.f[,2])
   
   N_T <- length(NEE.v) #number of observations(rows) in a T class
-  Ust_bin_size <- round(N_T/ctrluStarEst.l$uStarClasses)
+  Ust_bin_size <- round(N_T/ctrlUstarEst.l$UstarClasses)
   
-  #set up data frame for bin averages (by uStar)
-  Ust_bins.f <- matrix(NA,ctrluStarEst.l$uStarClasses,2)
+  #set up data frame for bin averages (by Ustar)
+  Ust_bins.f <- matrix(NA,ctrlUstarEst.l$UstarClasses,2)
   Ust_bins.f <- data.frame(Ust_bins)
   names(Ust_bins.f)[1]="Ust_avg"; names(Ust_bins.f)[2]="NEE_avg";
   #names(Ust_bins.f)[3]="N";
   
   #calculate u* bin averages
-  for (u in 1:ctrluStarEst.l$uStarClasses){
+  for (u in 1:ctrlUstarEst.l$UstarClasses){
     #/om:this part only implemented for checking C code compatibility...
     #ust_class_start = ust_class_end
     #ust_class_end = ust_class_start + Ust_bin_size-1
     #dataUstclass <- dataTclass[ust_class_start:ust_class_end,]
     #/eom    
-    if (u==ctrluStarEst.l$uStarClasses){ 
+    if (u==ctrlUstarEst.l$UstarClasses){ 
       # use end index of vector for slightly smaller last bin (due to rounding) 
       dsUstClass.f <- ds.f[((u-1)*Ust_bin_size+1):N_T,]
     }
@@ -194,8 +194,8 @@ estuStarThresholdSingleFw1Binned <- function(
       dsUstClass.f <- ds.f[((u-1)*Ust_bin_size+1):((u)*Ust_bin_size),]
     }
     #TODO: merge two following lines to one
-    Ust_bins.f$NEE_avg[u] <- mean(dsUstClass.f[,1], na.rm=T) #mean of NEE over uStar bins
-    Ust_bins.f$Ust_avg[u] <- mean(dsUstClass.f[,2], na.rm=T) #mean of uStar bins
+    Ust_bins.f$NEE_avg[u] <- mean(dsUstClass.f[,1], na.rm=T) #mean of NEE over Ustar bins
+    Ust_bins.f$Ust_avg[u] <- mean(dsUstClass.f[,2], na.rm=T) #mean of Ustar bins
     #Ust_bins.f$N[u] <- sum(!is.na(dataUstclass$nee))
   }
   
@@ -207,72 +207,72 @@ estuStarThresholdSingleFw1Binned <- function(
   # in order to avoid infinite loop in case of error
   # optimize with Thomas?
   while (!flag){ #only stop if threshold is found
-    if (!flag & (Ust_bins.f$NEE_avg[u] >= (ctrluStarEst.l$plateauCrit*mean(Ust_bins.f$NEE_avg[(u+1):(u+ctrluStarEst.l$ustPlateauFwd)],na.rm=T)))){ #na.rm=T to exclude NAs out of bounds..
+    if (!flag & (Ust_bins.f$NEE_avg[u] >= (ctrlUstarEst.l$plateauCrit*mean(Ust_bins.f$NEE_avg[(u+1):(u+ctrlUstarEst.l$ustPlateauFwd)],na.rm=T)))){ #na.rm=T to exclude NAs out of bounds..
       #   NEE_i >= .95*avg(i,i+1,...,i+10)  [FW]    
-      uStarThSingle <- Ust_bins.f$Ust_avg[u]        
+      UstarThSingle <- Ust_bins.f$Ust_avg[u]        
       flag <- TRUE #set flag for threshold found in this mode
     }          
     #case that no threshold could be found by plateau method, use maximum u* in that T_class...
-    if (u==(ctrluStarEst.l$uStarClasses-1)){ #FW1: -1 ; FW2: 
-      uStarThSingle <- Ust_bins.f$Ust_avg[u+1]        
+    if (u==(ctrlUstarEst.l$UstarClasses-1)){ #FW1: -1 ; FW2: 
+      UstarThSingle <- Ust_bins.f$Ust_avg[u+1]        
       break;      
     }
     u <- u+1 #increase index by 1
   }  
-  return(uStarThSingle)
+  return(UstarThSingle)
 }
 
 
 
-# TODO: restructure? may not be the most efficient cause uStar bin averages have to be computed twice (or more depending on how many methods).
-# TODO: wrap rest of uStar code from fw1 around the while loop... (if necessary, depending on structure)
-estuStarThresholdSingleFw2Binned <- function(
-  ### estimate the uStar threshold for single subset, using FW2 algorithm  	
-  Ust_bins.f			##<< data.frame with column s NEE_avg and Ust_avg, of NEE by uStar bins by \code{\link{binuStar}}
-  ,ctrluStarEst.l = controluStarEst()
+# TODO: restructure? may not be the most efficient cause Ustar bin averages have to be computed twice (or more depending on how many methods).
+# TODO: wrap rest of Ustar code from fw1 around the while loop... (if necessary, depending on structure)
+estUstarThresholdSingleFw2Binned <- function(
+  ### estimate the Ustar threshold for single subset, using FW2 algorithm  	
+  Ust_bins.f			##<< data.frame with column s NEE_avg and Ust_avg, of NEE by Ustar bins by \code{\link{binUstar}}
+  ,ctrlUstarEst.l = controlUstarEst()
 ){  
   # algorithm to check when plateau is reached
   flag <- FALSE
   #for every u* bin compare to avg of subsequent UST_PLATEAU, until found
   u <- 1
   while (!flag){ #only stop if threshold is found
-    if (!flag & (Ust_bins.f$NEE_avg[u] >= (ctrluStarEst.l$plateauCrit*mean(Ust_bins.f$NEE_avg[(u+1):(u+ctrluStarEst.l$ustPlateauFwd)],na.rm=T))) 
-        & (Ust_bins.f$NEE_avg[u+1] >= (ctrluStarEst.l$plateauCrit*mean(Ust_bins.f$NEE_avg[(u+1+1):(u+ctrluStarEst.l$ustPlateauFwd+1)],na.rm=T)))){ 
-      uStarThSingle <- Ust_bins.f$Ust_avg[u]        
+    if (!flag & (Ust_bins.f$NEE_avg[u] >= (ctrlUstarEst.l$plateauCrit*mean(Ust_bins.f$NEE_avg[(u+1):(u+ctrlUstarEst.l$ustPlateauFwd)],na.rm=T))) 
+        & (Ust_bins.f$NEE_avg[u+1] >= (ctrlUstarEst.l$plateauCrit*mean(Ust_bins.f$NEE_avg[(u+1+1):(u+ctrlUstarEst.l$ustPlateauFwd+1)],na.rm=T)))){ 
+      UstarThSingle <- Ust_bins.f$Ust_avg[u]        
       flag <- TRUE #set flag for threshold found in this mode
 
     }    
     #case that no threshold could be found by plateau method, use maximum u* in that T_class...
     if (u==(nrow(Ust_bins.f)-2)){ #FW1: -1 ; FW2: 
-      uStarThSingle <- Ust_bins.f$Ust_avg[u+1]        
+      UstarThSingle <- Ust_bins.f$Ust_avg[u+1]        
       break;      
     }
     u <- u+1 #increase index by 1
   }   
-  return(uStarThSingle)    
+  return(UstarThSingle)    
 }
 
-estuStarThresholdYear <- function(
-  ### estimate the uStar threshold by aggregating estimates for singe seasonal and temperature classes
+estUstarThresholdYear <- function(
+  ### estimate the Ustar threshold by aggregating estimates for singe seasonal and temperature classes
   ds						    ##<< data.frame with columns
-  ,uStarColName = "uStar"		##<< uStar
+  ,UstarColName = "Ustar"		##<< Ustar
   ,NEEColName = "NEE"
   ,tempColName = "airTemp"
   ,seasonFactor.v = ds[,"season"]	##<< [from Thomas] TODO calculate seasons by default to month
-  ,ctrluStarEst.l = controluStarEst()
+  ,ctrlUstarEst.l = controlUstarEst()
 ){
-  ##references<< uStar filtering following the idea in Papale 2006	
+  ##references<< Ustar filtering following the idea in Papale 2006	
   # subset for first season
   # TODO: implement seasonal functionality
   dsi <- subset(ds, seasonFactor.v == 1)
   dsiSort <- arrange(dsi, dsi[,tempColName]) #sort values in a season by air temperature
   N <- length(dsi[,NEEColname]) #number of observations (rows) total, probably can get from elsewhere..
-  T_bin_size <- round(N/ctrluStarEst.l$taClasses) #set T_bin size so that every bin has equal #values
+  T_bin_size <- round(N/ctrlUstarEst.l$taClasses) #set T_bin size so that every bin has equal #values
   
-  #set up vector that contains uStar values for temperature classes
-  uStarTh.v = vector(length=ctrluStarEst.l$taClasses)
+  #set up vector that contains Ustar values for temperature classes
+  UstarTh.v = vector(length=ctrlUstarEst.l$taClasses)
   
-  for (k in 1:ctrluStarEst.l$taClasses){
+  for (k in 1:ctrlUstarEst.l$taClasses){
     #original Dario's C version...
     #ta_class_start = 0;
     #ta_class_end = season_start_index;
@@ -286,7 +286,7 @@ estuStarThresholdYear <- function(
     #/eom
     
     #subset into Ta classes
-    if (k==ctrluStarEst.l$taClasses){ # use end index of vector for slightly smaller last bin (due to rounding) 
+    if (k==ctrlUstarEst.l$taClasses){ # use end index of vector for slightly smaller last bin (due to rounding) 
       dsiSortTclass <- dsiSort[((k-1)*T_bin_size+1):N,]
     }
     else {
@@ -294,68 +294,68 @@ estuStarThresholdYear <- function(
     }
     
     #constraint: u* threshold only accepted if T and u* are not or only weakly correlated..
-    Cor1 = abs(cor(dsiSortTclass[,uStarColName],dsiSortTclass[,tempColName]))
+    Cor1 = abs(cor(dsiSortTclass[,UstarColName],dsiSortTclass[,tempColName]))
     # TODO: check more correlations here? [check C code]
-    #      Cor2 = abs(cor(dataMthTsort$uStar,dataMthTsort$nee))
+    #      Cor2 = abs(cor(dataMthTsort$Ustar,dataMthTsort$nee))
     #      Cor3 = abs(cor(dataMthTsort$tair,dataMthTsort$nee))
-    if (Cor1 < ctrluStarEst.l$corrCheck){ #& Cor2 < CORR_CHECK & Cor3 < CORR_CHECK){
-      uStarTh.v[k]=estuStarThresholdSingle( dsiSortTclass[,NEEColName], dsiSortTclass[,uStarColName], ctrluStarEst.l = ctrluStarEst.l)
+    if (Cor1 < ctrlUstarEst.l$corrCheck){ #& Cor2 < CORR_CHECK & Cor3 < CORR_CHECK){
+      UstarTh.v[k]=estUstarThresholdSingle( dsiSortTclass[,NEEColName], dsiSortTclass[,UstarColName], ctrlUstarEst.l = ctrlUstarEst.l)
     }
     else { #correlation between T and u* too high
       #fill respective cell with NA
-      uStarTh.v[k] = NA
+      UstarTh.v[k] = NA
       #TODO: should a message be printed here to the user??
     }
   }
   #OUTPUT:
-  # This function returns one single uStar threshold value
+  # This function returns one single Ustar threshold value
   # i.e., the median over taClasses
-  return(median(uStarTh.v))
+  return(median(UstarTh.v))
 }
 
-estuStarThreshold <- function(
-		### apply estuStarThresold for each year in ds
+estUstarThreshold <- function(
+		### apply estUstarThresold for each year in ds
 		ds						    ##<< data.frame with columns
-		,...						##<< further arguments to \code{\link{estuStarThresholdYear}}
+		,...						##<< further arguments to \code{\link{estUstarThresholdYear}}
 		,seasonFactor.v = (as.POSIXlt(ds$DateTime)$mon-1) %/% 3	##<< factor of seasons so split dsYear
 		,yearFactor.v = as.POSIXlt(ds$DateTime)$year+1900	##<< factor vector (nrow(dsYear) of seasons so split dsYear 
 ){
 	##references<< inspired by Papale 2006
 	ds$season <- seasonFactor.v
 	if( !length( yearFactor.v) ){
-		c( '0' = estuStarThresholdYear(ds,...) )
+		c( '0' = estUstarThresholdYear(ds,...) )
 	}else{
 		ds$yearFac <- yearFactor.v
-		daply( ds, .(yearFactor.v), estuStarThresholdYear, ... )
+		daply( ds, .(yearFactor.v), estUstarThresholdYear, ... )
 	}
 	
-	##value<< a vector with uStar Threshold estiamtes. Names correspond to the year of the estimate 
+	##value<< a vector with Ustar Threshold estiamtes. Names correspond to the year of the estimate 
 }
-attr(estuStarThreshold ,"ex") <- function(){
+attr(estUstarThreshold ,"ex") <- function(){
 	Dir.s <- paste(system.file(package='REddyProc'), 'examples', sep='/')
 	EddyData.F <- fLoadTXTIntoDataframe('Example_DETha98.txt', Dir.s)
 	dss <- subset(EddyData.F, DoY >= 150 & DoY <= 250)
 	dss2 <- dss; dss2$Year <- 1999
 	EddyDataWithPosix.F <- ds <- fConvertTimeToPosix(rbind(dss,dss2), 'YDH', Year.s='Year', Day.s='DoY', Hour.s='Hour')
-	(res <- estuStarThreshold(ds))
+	(res <- estUstarThreshold(ds))
 }
 
 
-estuStarThresholdDistribution <- function(
-		### Estimating the uStarTrhesholdDistribution by bootstrapping over data
+estUstarThresholdDistribution <- function(
+		### Estimating the UstarTrhesholdDistribution by bootstrapping over data
 		ds					    ##<< data.frame with columns
-		,...					##<< further arguments to \code{\link{estuStarThreshold}}
+		,...					##<< further arguments to \code{\link{estUstarThreshold}}
 		,nSample = 100
         ,probs = c(0.05,0.5,0.95)
 ){
-		uStar.l <- boot( ds, estuStarThreshold, nSample)
+		Ustar.l <- boot( ds, estUstarThreshold, nSample)
         
-        ##value<< a matrix with with first column denoting the year and other columns correponsing to the quantiles of uStar estimate for given probabilities \code{probs}
+        ##value<< a matrix with with first column denoting the year and other columns correponsing to the quantiles of Ustar estimate for given probabilities \code{probs}
 		years = c(2001,2002)
 		#years = c(2001)
-		uStarQuantiles = matrix( probs, byrow=TRUE, ncol=length(probs), nrow=length(years), dimnames=list(NULL,probs))
+		UstarQuantiles = matrix( probs, byrow=TRUE, ncol=length(probs), nrow=length(years), dimnames=list(NULL,probs))
         res <- 
-				cbind( 	year=years, uStarQuantiles)             
+				cbind( 	year=years, UstarQuantiles)             
 }
 
 
