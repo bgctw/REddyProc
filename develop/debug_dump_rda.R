e5cc808d7ece163337719620e68205d59aa7cd4d
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2021-02-08 09:51:14 +0100

    custom color palette in fingerprint

---
+++ develop/debug_dump_rda.R
@@ -0,0 +1,11 @@
+
+dump_and_quit <- function() {
+  # Save debugging info to file last.dump.rda
+  dump.frames(to.file = TRUE)
+  # Quit R with error status
+  q(status = 1)
+}
+options(error = dump_and_quit)
+
+load("tmp/last.dump.rda")
+debugger(last.dump)
