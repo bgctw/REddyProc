f9c5987690404c65f1168fe78e6f8bb9763407f4
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2021-02-09 08:25:57 +0100

    fix docu of partGLControl
    did not parse documentation of argument fixedTempSens, if  code spanned several lines before or in docu

--- R/PartitioningLasslop10.R
+++ R/PartitioningLasslop10.R
@@ -343,14 +343,14 @@ partGLControl <- function(
 		  ## repeating estimation
 			## with \code{isNeglectVPDEffect = TRUE} trying to predict when VPD
 			## is missing
-		, fixedTempSens = data.frame(
-		  E0 = NA_real_, sdE0 = NA_real_, RRef = NA_real_)	##<< data.frame
+		, fixedTempSens = data.frame( ##<< data.frame
 		  ## of one row or nRow = nWindow
 			## corresponding to return value of \code{partGLFitNightTimeTRespSens}
 			## While column \code{RRef} is used only as a  prior and initial value for
 			## the daytime-fitting and can be NA,
 			## \code{E0} is used as given temperature sensitivity and varied according
 			## to \code{sdE0} in the bootstrap.
+		  E0 = NA_real_, sdE0 = NA_real_, RRef = NA_real_)	
 		, replaceMissingSdNEEParms = c(perc = 0.2, minSd = 0.7)	##<< parameters for
 		  ## replacing missing standard deviation of NEE.
 			## see \code{replaceMissingSdByPercentage}.
